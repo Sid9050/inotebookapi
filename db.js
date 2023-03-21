95733c3dda148d3823cfc36f3d94128f4520f18e
Sid9050 <sidchawla2015@gmail.com>
2023-03-21 18:31:24 +0530

    ready

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
 const mongoose =require('mongoose')
-const mongoUri='mongodb://27017/iNotebook'
+const mongoUri='mongodb://27017/iNotebookmongodb+srv://sidchawla2015:Sid123456@cluster0.2hcl3yq.mongodb.net/iNotebook?retryWrites=true&w=majority'
 
 
 
 const connectToMongo=()=>{
     mongoose.connect(mongoUri,{
     useNewUrlParser: true, 
-    useUnifiedTopology: true
+    useUnifiedTopology: true, 
+    serverApi: ServerApiVersion.v1
 })
 .then(db => console.log('DB is connected'))
 .catch(err => console.log(err));
