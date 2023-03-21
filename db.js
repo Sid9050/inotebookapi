a624c18e80584601803048469011e68a4c03f478
Sid9050 <sidchawla2015@gmail.com>
2023-03-21 18:55:28 +0530

    ready

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose =require('mongoose')
-const mongoUri='mongodb://27017/iNotebookmongodb+srv://sidchawla2015:Sid123456@cluster0.2hcl3yq.mongodb.net/iNotebook?retryWrites=true&w=majority'
+const mongoUri='mongodb+srv://sidchawla2015:Sid123456@cluster0.2hcl3yq.mongodb.net/?retryWrites=true&w=majority'
 
 
 
